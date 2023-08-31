9211253cb1f3bef9317bbc22a6d929967ba0241a
Aniruddh <adubge@gmail.com>
2023-08-31 12:43:31 +0530

    added genshin stats

--- commands/genshinStats.js
+++ commands/genshinStats.js
@@ -1,10 +1,22 @@
 const { SlashCommandBuilder } = require('discord.js');
+const { request } = require('undici');
+
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('genshinstat')
-		.setDescription('Gives stat of the genshin item requested'),
+		.setDescription('Gives stat of the genshin item requested')
+		.addStringOption(option =>
+			option
+				.setName('item')
+				.setDescription('Enter item you wanna search')
+				.setRequired(true)),
 	async execute(interaction) {
+		const query = interaction.options.getString('item');
+		const result = await request(`https://https://api.genshin.dev/characters/${query}`);
+		const { list } = await result.body.json();
+		
+		console.log(list);
 		return interaction.reply('u are gay!');
 	},
 };
